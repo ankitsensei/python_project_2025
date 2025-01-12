a660608cada04a32ca779c5209f4199790b37574
Ankit Bhagat <main.ankitbhagat@gmail.com>
2025-01-12 21:52:56 +0530

    checking git is working or not

--- blackjack21/main.py
+++ blackjack21/main.py
@@ -1,3 +1,19 @@
 import art
-print(art.blackjack)
+# print(art.blackjack)
+
+
+
+
+
+def deal(totalAmount):
+    while True:
+        hit = input("Want more cards [y/n]: ").lower()
+        if hit == 'y':
+            # Grab another card
+
+
+
+totalAmount = 2500
+deal(totalAmount)
+
 
