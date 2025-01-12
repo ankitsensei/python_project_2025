8dcef67a03fbbec52d576503ca3b557b5b576702
Ankit Bhagat <main.ankitbhagat@gmail.com>
2025-01-12 23:57:32 +0530

    resolving prob

--- blackjack21/main.py
+++ blackjack21/main.py
@@ -1,19 +1,44 @@
 import art
+import random
 # print(art.blackjack)
 
-
-
-
-
-def deal(totalAmount):
-    while True:
-        hit = input("Want more cards [y/n]: ").lower()
-        if hit == 'y':
-            # Grab another card
-
-
-
+cards = [1,2,3,4,5,6,7,8,9,10,11]
 totalAmount = 2500
-deal(totalAmount)
+
+
+
+def botRandomCard():
+    global cards
+    
+    randomCard = random.choice(cards)
+    
+    botTotalCard = []
+    botSumCard = 0
+    
+    for i in botTotalCard:
+        botSumCard +=i
+        
+    if botSumCard > 16:
+        # take more card
+        
+
+def deal():
+    global totalAmount
+    betAmount = int(input(f"Enter bet amount from  Rs.{totalAmount}: "))
+    if betAmount <= totalAmount:
+        while True:
+            hit = input("Want more cards [y/n]: ").lower()
+            if hit == 'y':
+                # Grab another card
+                print("Grabbing")
+            else: # If clicked stand
+                print()
+                break
+            
+    else:
+        print("You don't have enough Rs. to play 🥹")
+
+deal()
+
 
 
---
+++ rough.py
@@ -0,0 +1,22 @@
+import random
+
+cards = [1,2,3,4,5,6,7,8,9,10,11]
+
+def botRandomCard():
+    global cards
+    
+    botTotalCard = []
+    botCardSum = 0
+    
+    while botCardSum < 16:
+        # take more card
+        randomCard = random.choice(cards)
+        botTotalCard.append(randomCard)
+        for i in botTotalCard:
+            botCardSum += 1
+    print(botTotalCard)
+    print(botCardSum)
+    
+    
+botRandomCard()
+
